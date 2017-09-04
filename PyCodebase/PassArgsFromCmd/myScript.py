ba72893b6c177cead5b35701599a9dfc9c88be30
Ramanpreet Chand <rchand595@gmail.com>
2017-09-03 22:51:00 -0400

    commit changes to file directory

--- PyCodebase/PassArgsFromCmd/myScript.py
+++ PyCodebase/PassArgsFromCmd/myScript.py
@@ -1,5 +1,5 @@
-import sys # reads a list of arguments off the batch file, starting with the script name
 import os
+import sys  # reads a list of arguments off the batch file, starting with the script name
 
 scriptName = sys.argv[0] # script name is captured first, always
 index = (len(sys.argv)-1) # env argument is last in the list
